9094cc9aef1868bf4e49fa43707f157e63a71017
Artur Falborski <falborski.artur@gmail.com>
2017-03-26 11:29:31 +0200

    added game.js file

---
+++ js/game.js
@@ -0,0 +1,13 @@
+/**
+ * Created by arturfalborski on 26.03.17.
+ */
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
