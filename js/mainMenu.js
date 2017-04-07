bf443bc7ef821347a42a738a0d2699883ea50951
Artur Falborski <falborski.artur@gmail.com>
2017-04-07 18:59:07 +0200

    linked mainMenu.js with preload to site

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -4,6 +4,9 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+    game.load.image('title', 'assets/sprites/title.png');
+    game.load.image('helpButton', 'assets/sprites/helpbutton.png');
+    game.load.image('playButton', 'assets/sprites/playbutton.png');
 }
 
 function create() {
