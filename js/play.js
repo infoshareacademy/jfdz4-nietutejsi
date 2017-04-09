d9c60370f9d4b9f34a0c395362182f573d80c318
Rafal Paluch <ravpaluch@gmail.com>
2017-04-09 12:18:06 +0200

    kill player

--- js/play.js
+++ js/play.js
@@ -42,24 +42,32 @@ var playState = {
     },
 
     update: function() {
-
+        // game.physics.arcade.collide(player,dog);
+        game.physics.arcade.overlap(player, dog, killPlayer, null, this);
         player.body.velocity.set(0);
         if (cursors.left.isDown) {
             player.body.velocity.x = -100;
-            player.play('left');}
-            else if (cursors.right.isDown){
-                player.body.velocity.x = 100;
-            player.play('right');}
-            else if (cursors.up.isDown){
-                player.body.velocity.y = -100;
-            player.play('up');}
-            else if (cursors.down.isDown){
-                player.body.velocity.y = 100;
-            player.play('down');}else{
-                player.animations.stop();
-            player.frame = 0;}
-    };
-    function killPlayer (player, dog) {
-        player.kill();
-}
+            player.play('left');
+        }
+        else if (cursors.right.isDown) {
+            player.body.velocity.x = 100;
+            player.play('right');
+        }
+        else if (cursors.up.isDown) {
+            player.body.velocity.y = -100;
+            player.play('up');
+        }
+        else if (cursors.down.isDown) {
+            player.body.velocity.y = 100;
+            player.play('down');
+        } else {
+            player.animations.stop();
+            player.frame = 0;
+        }
+    }
 };
+
+
+function killPlayer (player, dog) {
+    player.kill();
+}
