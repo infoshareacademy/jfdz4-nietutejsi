0adbbf561bdf7a621c15ff317a124676286dc716
Artur Falborski <falborski.artur@gmail.com>
2017-04-23 09:52:34 +0200

    added instruction state

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-handler');
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
+game.state.add('instruction', instructionState);
 game.state.add('play', playState);
 game.state.add('win', winState);
 game.state.add('lose', loseState);
