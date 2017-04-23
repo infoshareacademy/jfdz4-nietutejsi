52dcd91fca089c22100c7ce0013548327b807d95
Artur Falborski <falborski.artur@gmail.com>
2017-04-23 11:36:09 +0200

    removed unnecessary comment

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
 $( document ).ready(function() {
-$('#sign-up-button').click(function () {
-    var x = document.forms["signupForm"]["email"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        alert("Nieprawidłowy adres e-mail");
-    } else if(document.signupForm.agreement.checked)
-    {
-        $('.users-email').val('');
-        $('#game-handler').removeClass('game-handler-hidden');
-        $('#game-handler').addClass('game-handler-shown');
-        return false;
-
-    function validateEmail() {
+    $('#sign-up-button').click(function () {
         var x = document.forms["signupForm"]["email"].value;
         var atpos = x.indexOf("@");
         var dotpos = x.lastIndexOf(".");
-        if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+        if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
             alert("Nieprawidłowy adres e-mail");
+        } else if(document.signupForm.agreement.checked)
+        {
+            $('.users-email').val('');
+            $('#game-handler').removeClass('game-handler-hidden');
+            $('#game-handler').addClass('game-handler-shown');
             return false;
         }
-    }
-
-    function checkTerms() {
-        if (document.signupForm.agreement.checked) {
-            document.signupForm.submitemail.disabled = false;
-        }
-        else {
-            document.signupForm.submitemail.disabled = true;
+        else
+        {
+            document.signupForm.submitemail.disabled=true;
             alert("Brak wyrażenia zgody");
         }
-    }
-
-    function enableSubmit() {
-        document.signupForm.submitemail.disabled = false
-    }
-});
-
+    });
     $(window).on("scroll", function() {
 
         var currentPos = $(window).scrollTop();
@@ -55,3 +36,4 @@ $('#sign-up-button').click(function () {
             }
         });
     });
+});
