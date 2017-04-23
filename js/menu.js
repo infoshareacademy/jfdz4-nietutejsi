3552495a033ff07d1b8f00e3d738e0ba3a53d84f
Artur Falborski <falborski.artur@gmail.com>
2017-04-23 09:48:39 +0200

    improved menu state with buttons

--- js/menu.js
+++ js/menu.js
@@ -2,22 +2,27 @@ var menuState = {
 
     create: function () {
         
+        var startButton = game.add.button(301, game.world.height - 160, 'beginGameButton', gameStartOnClick, this);
+        var instructionButton = game.add.button(301, game.world.height - 115, 'instructionButton', instructionStartOnClick);
         var nameLabel = game.add.text(80, 80, 'WishBook',
           { font: '50px Arial', fill: '#ffffff' });
 
         var undernameLabel = game.add.text(250, 138, 'the Game',
           { font: '32px Arial', fill: '#ffffff'});
-        
-        var startLabel = game.add.text(80, game.world.height-80,
-          'Naciśnij "W" by zacząć grę!',
-          {font: '25px Arial', fill: '#ffffff' });
-        
-        var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-        
-        wkey.onDown.addOnce(this.start, this);
     },
     
     start: function () {
         game.state.start('play');
+    },
+    instruction: function() {
+        game.state.start('instruction');
     }
 };
+
+function gameStartOnClick() {
+    menuState.start();
+}
+
+function instructionStartOnClick() {
+    menuState.instruction();
+}
