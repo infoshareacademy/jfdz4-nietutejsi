091f461769fecfd4140adabacceab789e82f04c5
Artur Falborski <falborski.artur@gmail.com>
2017-04-23 09:22:26 +0200

    put game into game handler

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-handler');
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
