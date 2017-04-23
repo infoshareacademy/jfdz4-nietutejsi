0ba5414541992e51f6a395db99744cf9c934df96
Artur Falborski <falborski.artur@gmail.com>
2017-04-23 12:47:55 +0200

    improved win state

--- js/win.js
+++ js/win.js
@@ -4,17 +4,15 @@ var winState = {
 
     var winLabel = game.add.text(80, 80, 'WYGRAŁEŚ!',
       {font: '50px Arial', fill: '#00FF00'});
-    
-    var startLabel = game.add.text(80, game.world.height - 80,
-      'Naciśnij "W" by rozpocząć grę od nowa',
-      {font: '25px Arial', fill: '#ffffff'});
-    
-    var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-    
-    wkey.onDown.addOnce(this.restart, this);
+
+    replayButton = game.add.button(301, game.world.height - 160, 'replayButton', actionOnClick, this);
   },
   
   restart: function () {
     game.state.start('menu');
   }
 };
+
+function actionOnClick() {
+  winState.restart();
+}
