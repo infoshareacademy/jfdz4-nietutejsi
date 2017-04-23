5a881ef672787705e217d31d5e6c70016f579039
anik-20 <anik20@wp.pl>
2017-04-23 23:03:19 +0200

    część instrukcji do gry

--- js/instruction2.js
+++ js/instruction2.js
@@ -5,10 +5,13 @@ var instructionState2 = {
 
     create: function () {
 
-        game.add.text(330, 50, 'POSTACIE', {
+        game.add.text(330, 50, 'PRZEBIEG GRY', {
             font: '50px Arial', fill: '#ffffff'
         });
-        game.add.text(10, 100, ' najwięcej\n punktów.', {
+        game.add.text(10, 100, 'Gracz porusza listonoszem po planszy za pomocą klawiszy strzałek: góra, dół, lewo, prawo.\n ' +
+            'Unika psa. Zbiera koperty pojawiąjące się w losowych miejscach planszy. Przesuwa się w strone odbiorcy,\n ' +
+            'który też pojawia się w losowym polu planszy. W ciągu 60 sekund, listonosz musi zebrać jak najwięcej kopert.\n ' +
+            'Jednak, uwaga, im więcej kopert zbierze, tym wolniej porusza się....', {
             font: '30px Arial', fill: '#ffffff'
         });
 
@@ -17,10 +20,10 @@ var instructionState2 = {
     },
 
     backToMenu: function () {
-        game.state.start('menu');
+        game.state.start('instruction1');
     },
     moveToNextInstruction: function() {
-        game.state.start('instruction1');
+        game.state.start('instruction3');
     }
 };
 
