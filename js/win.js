bac5c09a3bf869d40dcada62b8327e9e3c379346
Artur Falborski <falborski.artur@gmail.com>
2017-05-04 21:26:15 +0200

    scaled win.js file

--- js/win.js
+++ js/win.js
@@ -2,10 +2,11 @@ var winState = {
 
   create: function () {
 
-    var winLabel = game.add.text(100, 80, 'W ciągu 60 sekund udało ci się zdobyć ' + scoreValue + ' punktów!',
+    var winLabel = game.add.text(Math.round(gameWidth * 0.125), Math.round(gameHeight * 0.13333333333), 'W ciągu 60 sekund udało ci się zdobyć ' + scoreValue + ' punktów!',
       {font: '24px Arial', fill: '#00FF00'});
-
-    replayButton = game.add.button(301, game.world.height - 160, 'replayButton', actionOnClick, this);
+      winLabel.scale.setTo(gameWidthScale, gameHeightScale);
+    replayButton = game.add.button(Math.round(gameWidth * 0.37625), Math.round(game.world.height - (gameHeight * 0.26666666666)), 'replayButton', actionOnClick, this);
+      replayButton.scale.setTo(gameWidthScale, gameHeightScale);
   },
   
   restart: function () {
