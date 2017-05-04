a8e0074155a3f216f14dbb49c4b744eadb063281
Artur Falborski <falborski.artur@gmail.com>
2017-05-04 21:22:22 +0200

    scaled lose.js file

--- js/lose.js
+++ js/lose.js
@@ -2,11 +2,14 @@ var loseState = {
 
   create: function () {
     
-    replayButton = game.add.button(301, game.world.height - 160, 'replayButton', actionOnClick, this);
+    replayButton = game.add.button(Math.round(gameWidth * 0.37625), Math.round(game.world.height - (gameHeight * 0.26666666666)), 'replayButton', actionOnClick, this);
+      replayButton.scale.setTo(gameWidthScale, gameHeightScale);
 
-    var loseLabel = game.add.text(80, 80, 'Przegrałeś :(',
+    var loseLabel = game.add.text(Math.round((gameWidth * 0.1)), Math.round((gameHeight * 0.13333333333)), 'Przegrałeś :(',
       {font: '50px Arial', fill: '#00FF00'});
+     loseLabel.scale.setTo(gameWidthScale, gameHeightScale);
   },
+
 
   restart: function () {
     game.state.start('menu');
