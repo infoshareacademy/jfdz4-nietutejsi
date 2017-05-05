3fb05209b62aa088dcc92d58cfa7b5799e4f40a6
Artur Falborski <falborski.artur@gmail.com>
2017-05-05 18:26:36 +0200

    scaled instruction3.js file

--- js/instruction3.js
+++ js/instruction3.js
@@ -2,14 +2,18 @@ var instructionState3 = {
 
     create: function () {
 
-        game.add.text(330, 8, 'CEL GRY', {
+        gameDestinationR = game.add.text((gameWidth * 0.4125), (gameHeight * 0.01333333333), 'CEL GRY', {
             font: '30px Arial', fill: '#ffffff'
         });
-        game.add.text(8, 50, 'Zadaniem gracza jest w ciągu 60 sekund zebranie kopert, przekazanie ich odbiorcy\n i zgromadzenie, jak najwięcej punktów.', {
+        gameDestinationR.scale.setTo(gameWidthScale, gameHeightScale);
+
+        playerRoleR = game.add.text((gameWidth * 0.01), (gameHeight * 0.08333333333), 'Zadaniem gracza jest w ciągu 60 sekund zebranie kopert, przekazanie ich odbiorcy\n i zgromadzenie, jak najwięcej punktów.', {
             font: '20px Arial', fill: '#ffffff'
         });
+        playerRoleR.scale.setTo(gameWidthScale, gameHeightScale);
 
-        game.add.button(0, 0, 'jumpLeft', backMenu);
+        menuButtonR = game.add.button(0, 0, 'jumpLeft', backMenu);
+        menuButtonR.scale.setTo(gameWidthScale, gameHeightScale);
     },
 
     backToMenu: function () {
