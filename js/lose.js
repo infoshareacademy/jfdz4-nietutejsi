a22e365f9b6ef5bde99213028f6fdebf0717b760
Artur Falborski <falborski.artur@gmail.com>
2017-05-06 13:25:55 +0200

    changed position of the text and increased envelope burden

--- js/lose.js
+++ js/lose.js
@@ -5,8 +5,8 @@ var loseState = {
     replayButton = game.add.button(Math.round(gameWidth * 0.37625), Math.round(game.world.height - (gameHeight * 0.26666666666)), 'replayButton', actionOnClick, this);
       replayButton.scale.setTo(gameWidthScale, gameHeightScale);
 
-    var loseLabel = game.add.text(Math.round((gameWidth * 0.1)), Math.round((gameHeight * 0.13333333333)), 'Przegrałeś :(',
-      {font: '50px Arial', fill: '#00FF00'});
+    var loseLabel = game.add.text(Math.round((gameWidth * 0.325)), Math.round((gameHeight * 0.13333333333)), 'Przegrałeś :(',
+      {font: '50px Arial', fill: '#ffffff'});
      loseLabel.scale.setTo(gameWidthScale, gameHeightScale);
   },
 
